fb057b43e04be205b37809d7ab19bb2a294c17e1
vasukumar2001 <vasufadadu123@gmail.com>
2025-11-15 19:07:39 +0530

    add DiscountGrpc Service

---
+++ src/Services/Discount/Discount.Grpc/Data/Extentions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public static class Extentions
+    {
+        public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope() ;
+            using var Dbcontext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+            Dbcontext.Database.MigrateAsync();
+            
+            return app;
+        }
+    }
+}
